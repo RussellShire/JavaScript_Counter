384b258256ce2c2f0360c2964ea295622af841ea
RussellShire <russell.shire@gmail.com>
2022-07-11 15:57:39 +0100

    counter now working in basic form

--- main.js
+++ main.js
@@ -8,10 +8,25 @@ let counter = 0;
 // assigns the counter variable to the counter-value element within the html 
 document.querySelector(".counter-value").innerText = counter;
 
-
+// arrow function that loops through the actions array and creates results for each button
 actions.forEach(action => {
     action.addEventListener('click', () => {
+        //loops through the names of the data-action names in the html
         const type = action.dataset.action;
-        console.log(type);
-    })
-})
+        
+        // a switch where the cases are matched to the types from the button names
+        switch (type) {
+            case 'increase':
+                counter++;
+                break;
+            case 'decrease':
+                counter --;
+                break;
+            case 'reset':
+                counter = 0;
+                break;}
+
+        // Reassigns the counter to the html after the switch has manipulated the variable within JS
+        document.querySelector(".counter-value").innerText = counter;
+    });
+});
